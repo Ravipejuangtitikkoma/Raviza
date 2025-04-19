44d56784288518731025465fe818bdbdf1445c85
Ravipejuangtitikkoma <raviarmanza149@gmail.com>
2025-04-19 18:47:19 +0700

    perubahan yang ketiga

--- belajar.php
+++ belajar.php
@@ -1,4 +1,5 @@
 <?php 
+
 class mobil{
     public $warna;
     public $kendaraan;
@@ -6,7 +7,7 @@ class mobil{
     public $harga;
     public $kecepatan;
 
-    function _construct($warna,$kendaraan,$merk,$harga,$kecepatan)
+    function __construct($warna,$kendaraan,$merk,$harga,$kecepatan)//yang paling penting adalah __construnct garis bawahnya harus dua karena itu adalah constructor
     {
         $this->warna=$warna;
         $this->kendaraan=$kendaraan;
@@ -16,18 +17,29 @@ class mobil{
     }
 
     function info(){
-        return "mobil ini berwarna {$this->warna}<br>,kendaraan{$this->kendaraan}<br>,merk{$this->merk}<br>,harga{$this->harga}<br>,kecepetan{$this->kecepatan}<br>";
+        return "mobil ini berwarna {$this->warna}, kendaraan: {$this->kendaraan}, merk: {$this->merk}, harga: {$this->harga}, kecepatan: {$this->kecepatan} km/jam";
     }
+    
 
     function tambahKecepatan($kecepatan){
         $this->kecepatan += $kecepatan;
         return "kecepatan mobil sekarang adalah{$this->kecepatan} km/jam";
     }
+    
 }
 
-$mobil1=new mobil("merah","toyota","honda",12000,0);
-echo $mobil1->info();
+$mobil1 = new mobil("merah", "mobil", "honda", 12000, 0);
+$mobil2 = new mobil("biru", "motor", "yamaha", 15000, 0);
+echo $mobil1->info(); // ini yang menampilkan deskripsi mobil
+echo "<br>";
 echo $mobil1->tambahKecepatan(120);
+echo "<br>";
+//ini untuk menamilkan motor 2 dan kecepatan motor 2
+echo $mobil2->info();
+echo "<br>";
+echo $mobil2->tambahKecepatan(100);
+
+
 
 
 
